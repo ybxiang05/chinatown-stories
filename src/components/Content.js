7be5785fbc1ffa2fdd730d716fe5ec49a6bfea2b
Brooke <brooke.xiang@gmail.com>
2020-12-08 17:41:14 -0800

    add all articles

--- src/components/Content.js
+++ src/components/Content.js
@@ -53,6 +53,15 @@ const Content = props => {
     setOpen(!open);
   };
   const kamWai = interviews[0];
+  const massyBooks = interviews[1];
+  const baoBei = interviews[2];
+  const mrsKuang = interviews[3];
+  const emberWorks = interviews[4];
+  const corningDrugs = interviews[5];
+  const chauLuen = interviews[6];
+  const bambooVillage = interviews[7];
+  const houseOfRice = interviews[8];
+  const mrsLi = interviews[9];
   const pearlLow = interviews[10];
 
   //const MassyBooks = useInterviewContent();
@@ -65,16 +74,118 @@ const Content = props => {
         <Interview
           title={kamWai.title}
           subtitle={kamWai.subtitle}
+          blurb={kamWai.blurb}
           content={kamWai.content}
           backgrounds={kamWai.backgrounds}
           links={kamWai.links}
           italicized={kamWai.italicized}
         />
       </KamWai>
+      <div>
+        <Interview
+          title={massyBooks.title}
+          subtitle={massyBooks.subtitle}
+          blurb={massyBooks.blurb}
+          content={massyBooks.content}
+          backgrounds={massyBooks.backgrounds}
+          links={massyBooks.links}
+          italicized={massyBooks.italicized}
+        />
+      </div>
+      <div>
+        <Interview
+          title={baoBei.title}
+          subtitle={baoBei.subtitle}
+          blurb={baoBei.blurb}
+          content={baoBei.content}
+          backgrounds={baoBei.backgrounds}
+          links={baoBei.links}
+          italicized={baoBei.italicized}
+        />
+      </div>
+      <div>
+        <Interview
+          title={mrsKuang.title}
+          subtitle={mrsKuang.subtitle}
+          blurb={mrsKuang.blurb}
+          content={mrsKuang.content}
+          backgrounds={mrsKuang.backgrounds}
+          links={mrsKuang.links}
+          italicized={mrsKuang.italicized}
+        />
+      </div>
+      <div>
+        <Interview
+          title={emberWorks.title}
+          subtitle={emberWorks.subtitle}
+          blurb={emberWorks.blurb}
+          content={emberWorks.content}
+          backgrounds={emberWorks.backgrounds}
+          links={emberWorks.links}
+          italicized={emberWorks.italicized}
+        />
+      </div>
+      <div>
+        <Interview
+          title={corningDrugs.title}
+          subtitle={corningDrugs.subtitle}
+          blurb={corningDrugs.blurb}
+          content={corningDrugs.content}
+          backgrounds={corningDrugs.backgrounds}
+          links={corningDrugs.links}
+          italicized={corningDrugs.italicized}
+        />
+      </div>
+      <div>
+        <Interview
+          title={chauLuen.title}
+          subtitle={chauLuen.subtitle}
+          blurb={chauLuen.blurb}
+          content={chauLuen.content}
+          backgrounds={chauLuen.backgrounds}
+          links={chauLuen.links}
+          italicized={chauLuen.italicized}
+        />
+      </div>
+      <div>
+        <Interview
+          title={bambooVillage.title}
+          subtitle={bambooVillage.subtitle}
+          blurb={bambooVillage.blurb}
+          content={bambooVillage.content}
+          backgrounds={bambooVillage.backgrounds}
+          links={bambooVillage.links}
+          italicized={bambooVillage.italicized}
+        />
+      </div>
+      <div>
+        <Interview
+          title={houseOfRice.title}
+          subtitle={houseOfRice.subtitle}
+          blurb={houseOfRice.blurb}
+          content={houseOfRice.content}
+          backgrounds={houseOfRice.backgrounds}
+          links={houseOfRice.links}
+          italicized={houseOfRice.italicized}
+        />
+      </div>
+      <div>
+        <Interview
+          title={mrsLi.title}
+          subtitle={mrsLi.subtitle}
+          blurb={mrsLi.blurb}
+          content={mrsLi.content}
+          backgrounds={mrsLi.backgrounds}
+          links={mrsLi.links}
+          italicized={mrsLi.italicized}
+        />
+      </div>
+
       <PearlLow>
         <Interview
           title={pearlLow.title}
           subtitle={pearlLow.subtitle}
+          blurb={pearlLow.blurb}
           content={pearlLow.content}
           backgrounds={pearlLow.backgrounds}
           links={pearlLow.links}
--- src/components/Interview.js
+++ src/components/Interview.js
@@ -1,7 +1,39 @@
 import React from "react";
 import styled from "styled-components";
+import { Collapse, List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
 
-const Interview = ({ title, subtitle, content, backgrounds, links, italicized }) => {
+const StyledListItemText = styled(ListItemText)`
+  .MuiListItemText-root {
+    justify-content: center;
+    margin-left: 200px;
+  }
+  .MuiListItemText-multiline {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+const StyledListItem = styled(ListItem)`
+  display: flex;
+  flex-direction: column;
+  //   height: 80vh;
+  //   overflow: scroll;
+
+  p {
+    margin: 0 20%;
+  }
+`;
+
+const StyledInterview = styled.div`
+  padding: 0 20%;
+`;
+const InterviewPara = styled.p`
+  padding: 10px;
+`;
+
+const Interview = ({ title, subtitle, blurb, content, backgrounds, links, italicized }) => {
   const modifyContent = () => {
     //use find (and map if there are multiple italicized words) to match italicized word list against text inside content, if found, then paste in <span></span>
   };
@@ -12,19 +44,43 @@ const Interview = ({ title, subtitle, content, backgrounds, links, italicized })
   };
 
   return (
-    <div>
-      <div onClick={handleClick}>
-        <h3>{title}</h3>
-        <h4>{subtitle}</h4>
-      </div>
-      {open ? (
-        <div>
-          {contentText.map(paragraph => (
-            <p>{paragraph}</p>
-          ))}
-        </div>
-      ) : null}
-    </div>
+    // <div>
+    //   <div onClick={handleClick}>
+    //     <h3>{title}</h3>
+    //     <h4>{subtitle}</h4>
+    //   </div>
+    //   {open ? (
+    //     <div>
+    //       <p dangerouslySetInnerHTML={{ __html: blurb }}></p>
+    //       <div>
+    //         {contentText.map(paragraph => (
+    //           <p dangerouslySetInnerHTML={{ __html: paragraph }} key={paragraph}></p>
+    //         ))}
+    //       </div>
+    //     </div>
+    //   ) : null}
+    // </div>
+    <>
+      <StyledInterview>
+        <ListItem button onClick={handleClick}>
+          <StyledListItemText primary={title} secondary={subtitle} />
+          {open ? <ExpandLess /> : <ExpandMore />}
+        </ListItem>
+      </StyledInterview>
+      <Collapse in={open} unmountOnExit>
+        <List component="div">
+          <StyledListItem button>
+            <ListItemIcon />
+            <p dangerouslySetInnerHTML={{ __html: blurb }} />
+            <div>
+              {contentText.map(paragraph => (
+                <InterviewPara key={paragraph} dangerouslySetInnerHTML={{ __html: paragraph }} />
+              ))}
+            </div>
+          </StyledListItem>
+        </List>
+      </Collapse>{" "}
+    </>
   );
 };
 
